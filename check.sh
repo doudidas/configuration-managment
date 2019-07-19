78877cb46ddbef5246efa9ec30f03c81051647e5
Edouard Topin <ed.topin@gmail.com>
2019-07-19 15:41:52 +0200

    typo

--- check.sh
+++ check.sh
@@ -2,7 +2,7 @@
 platform="development"
 git diff $platform-current..remotes/origin/$platform-reference -- export/$1 > /tmp/diff;
 if [[ -n $(cat /tmp/diff) ]]; then
-    cat /tmp/diff
+    cat /tmp/diff;
     exit 1
 else
     exit 0
