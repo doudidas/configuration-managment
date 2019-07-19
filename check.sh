1948ffbd5b8690a99b42ad8e6c61b7070e190183
Edouard Topin <ed.topin@gmail.com>
2019-07-19 16:58:06 +0200

    test

--- check.sh
+++ check.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-git diff $platform-current..remotes/origin/$platform-reference -- export/$1 | grep -e "^\+  .*" -e "^\-  .*" -e "\"Id.*" > /tmp/diff
+currentBranch=$(git rev-parse --abbrev-ref HEAD)
+echo $currentBranch
+git diff $currentBranch..remotes/origin/$2-reference -- export/$1/*
+git diff $currentBranch..remotes/origin/$2-reference -- export/$1/* | grep -e "^\+  .*" -e "^\-  .*" -e "\"Id.*" > /tmp/diff
 if [[ -n $(cat /tmp/diff) ]]; then
     cat /tmp/diff
     exit 1
