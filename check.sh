7238e51ec75beb3eb1cd6f89ae9d337859a859b6
Edouard Topin <ed.topin@gmail.com>
2019-07-19 18:21:24 +0200

    1000x mieux

--- check.sh
+++ check.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-currentBranch=$(git branch | grep \* | cut -d ' ' -f2)
+
 referenceBranch=remotes/origin/$2-reference
 
-git diff $referenceBranch..$currentBranch -- export/$1/* | grep -e "^\+  .*" -e "^\-  .*" -e "^\+++.*" -e "^\---.*"> diff/$1.txt
+git diff $referenceBranch -- export/$1/* | grep -e "^\+  .*" -e "^\-  .*" -e "^\+++.*" -e "^\---.*"> diff/$1.txt
 if [[ -n $(cat diff/$1.txt) ]]; then
     cat diff/$1.txt
     exit 1
