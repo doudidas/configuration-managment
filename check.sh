7c604ad8b2690b6df7fba09a66867c24587737c3
Edouard Topin <ed.topin@gmail.com>
2019-07-19 12:17:32 +0200

    version 3

---
+++ check.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+git diff $platform-current..remotes/origin/$platform-reference -- export/$1 > /tmp/diff
+if [[ -n $(cat /tmp/diff) ]]; then
+    cat /tmp/diff
+    exit 1
+else
+    exit 0
+fi
