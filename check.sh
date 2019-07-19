e18a23479fcee25911bb81a0ec059ef7efd818b5
Edouard Topin <ed.topin@gmail.com>
2019-07-19 18:04:56 +0200

    change cache

--- check.sh
+++ check.sh
@@ -2,8 +2,8 @@
 currentBranch=$2-current
 referenceBranch=remotes/origin/$2-reference
 
-git diff $referenceBranch..$currentBranch -- export/$1/* | grep -e "^\+  .*" -e "^\-  .*" -e "^\+++.*" -e "^\---.*"> /tmp/diff
-if [[ -n $(cat /tmp/diff) ]]; then
+git diff $referenceBranch..$currentBranch -- export/$1/* | grep -e "^\+  .*" -e "^\-  .*" -e "^\+++.*" -e "^\---.*"> /tmp/$1
+if [[ -n $(cat /tmp/$1) ]]; then
     cat /tmp/diff
     exit 1
 else
