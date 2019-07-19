eefa050f66e6210efda0819eaa5132b6ead7cd48
Edouard Topin <ed.topin@gmail.com>
2019-07-19 15:39:20 +0200

    minify

--- check.sh
+++ check.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-git diff $platform-current..remotes/origin/$platform-reference -- export/$1 > /tmp/diff
+platform="development"
+git diff $platform-current..remotes/origin/$platform-reference -- export/$1 > /tmp/diff;
 if [[ -n $(cat /tmp/diff) ]]; then
     cat /tmp/diff
     exit 1
