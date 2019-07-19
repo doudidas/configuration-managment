8d351f36332932ea3cad711fd968ffe1f461b2f2
Edouard Topin <ed.topin@gmail.com>
2019-07-19 17:09:00 +0200

    branch independant

--- check.sh
+++ check.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 currentBranch=$(git rev-parse --abbrev-ref HEAD)
-echo $currentBranch
-git diff $currentBranch..remotes/origin/$2-reference -- export/$1/*
-git diff $currentBranch..remotes/origin/$2-reference -- export/$1/* | grep -e "^\+  .*" -e "^\-  .*" -e "\"Id.*" > /tmp/diff
+remoteBranch=${currentBranch/current/reference}
+echo $remoteBranch
+git diff $currentBranch..$remoteBranch -- export/$1/* | grep -e "^\+  .*" -e "^\-  .*" -e "^\+++.*" -e "^\---.*"> /tmp/diff
 if [[ -n $(cat /tmp/diff) ]]; then
     cat /tmp/diff
     exit 1
