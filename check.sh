fbcf1560171702ea1188354ac36bc44e1f1a42c9
Edouard Topin <ed.topin@gmail.com>
2019-07-19 18:15:14 +0200

    branch

--- check.sh
+++ check.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-currentBranch=$2-current
+currentBranch=$(git branch | grep \* | cut -d ' ' -f2)
 referenceBranch=remotes/origin/$2-reference
 
 git diff $referenceBranch..$currentBranch -- export/$1/* | grep -e "^\+  .*" -e "^\-  .*" -e "^\+++.*" -e "^\---.*"> diff/$1.txt
