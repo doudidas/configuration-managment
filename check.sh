eca082c96617e90338879aca03c23408e2e3b04e
Edouard Topin <ed.topin@gmail.com>
2019-07-23 16:18:34 +0200

    v2

--- check.sh
+++ check.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-git diff $platform-current..remotes/origin/$platform-reference -- export/$1 > /tmp/diff
-if [[ -n $(cat /tmp/diff) ]]; then
-    cat /tmp/diff
+
+referenceBranch=remotes/origin/$2-reference
+
+git diff $referenceBranch -- export/$1/* | grep -e "^\+  .*" -e "^\-  .*" -e "^\+++.*" -e "^\---.*"> diff/$1.txt
+if [[ -n $(cat diff/$1.txt) ]]; then
+    cat diff/$1.txt
     exit 1
 else
     exit 0
