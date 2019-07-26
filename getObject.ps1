06068ff3b88a001a493daffc1d8b35f268cdb658
Edouard Topin <ed.topin@gmail.com>
2019-07-26 15:54:59 +0200

    new version

--- getObject.ps1
+++ getObject.ps1
@@ -3,13 +3,23 @@ param(
     [string]$cmd
 )
 
+# Connect to the source vRA instance
+Get-Content .cached_session.json | ConvertFrom-Json | Set-Variable vRAConnection
+
 Write-Output $cmd
+
 $elements = Invoke-Expression $cmd
 
+Write-Output $elements
+
 $folderPath = "export/$cmd"
+
 If (!(test-path $folderPath)) {
     New-Item -ItemType Directory -Force -Path $folderPath
+} else {
+    Remove-Item $folderPath/*.json
 }
+
 
 foreach ($element in $elements) {
     if (Get-Member -inputobject $element -name "Name" -Membertype Properties) {
