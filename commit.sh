483ee63e83de9b0c35637cfd2019b36606fedf5b
Edouard Topin <ed.topin@gmail.com>
2019-08-21 11:50:34 +0200

    version: 1.3

--- commit.sh
+++ commit.sh
@@ -4,13 +4,7 @@ majVersion=$(cat ./version.txt | cut -d'.' -f1)
 minVersion=$(cat ./version.txt | cut -d'.' -f2)
 
 response=$1
-while [[ $response =~ ^[^yYnN]$ ]]
-do
-    echo "Are you doing a major update ?: [y/n]"
-    read  response
-done
-
-if [[ "$response" =~ ^[^yY]$ ]];then
+if [[ "$response" == "major" ]];then
     majVersion=$((majVersion + 1))
     minVersion=0
 else
