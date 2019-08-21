2c9106bc30810b27e1299b676498f794501a5936
Edouard Topin <ed.topin@gmail.com>
2019-08-21 11:45:11 +0200

    version $majVersion.$minVersion

--- check.ps1
+++ check.ps1
@@ -87,7 +87,7 @@ try {
     }
 
     $overview += $tmp
-    $details | Format-Table | Out-String -Width 4096 | Out-File /tmp/$platform-$element.txt
+    $details | Format-Table | Out-String -Width 4096 | Out-File /var/log/jenkins/configuration-drift/$platform-$element.txt
     $details | ForEach-Object {
         $env = $_.Environment
         $name = $_.Name
---
+++ commit.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+git add --all
+
+majVersion=$(cat ./version.txt | cut -d'.' -f1)
+minVersion=$(cat ./version.txt | cut -d'.' -f2)
+
+response=$1
+while [[ $response =~ ^[^yYnN]$ ]]
+do
+    echo "Are you doing a major update ?: [y/n]"
+    read  response
+done
+
+if [ $response = "y" ]
+then
+    majVersion=$((majVersion + 1))
+    minVersion=0
+else
+    minVersion=$((minVersion + 1))
+fi
+
+git commit -m 'version $majVersion.$minVersion'
+
